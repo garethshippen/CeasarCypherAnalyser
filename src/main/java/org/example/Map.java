fc9f0ece78af29da27aa6475bc15b2f8befe0eba
Gareth Shippen <garethshippen@yahoo.co.uk>
2022-12-14 16:44:43 +0000

    Tidied files

--- src/main/java/org/example/Map.java
+++ src/main/java/org/example/Map.java
@@ -3,16 +3,21 @@ package org.example;
 public class Map
 {
     private final String letterFreq = "etaoinshrdlcumwfgypbvkjxqz";
-    public final static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private final int e2t = (int)'t' - (int)'e';
     private int[] frequencies;
-    private static final int e2t = (int)'t' - (int)'e';
     public Map(int[] _frequencies)
     {
         frequencies = _frequencies;
     }
 
-    public static int e2Tdist(int[] _frequencies) //the distance between first and second modal letter
+    /*
+    returns true or false on likelihood of being a rot cypher based on distance
+    between first two modal letters ie e and t or delta(et)
+    */
+    private boolean rotFilter(int[] _frequencies)
     {
+        boolean isRot = false;
         int[] frequencies = _frequencies;
         final int REPEATS = 2;
         int instances;
@@ -32,18 +37,14 @@ public class Map
                     modals[j] = i;
                 }
             }
-
             frequencies[modals[j]] = -1;
         }
-        System.out.printf("\'e\' is %d \'t\' is %d\n", modals[0], modals[1]);
-        if(modals[0] >= 0 && modals[1] >= 0)
-        {
-            return (length + modals[1] - modals[0]) % length;
-        }
-        else
-        {
-            return -1;
-        }
+
+        if(modals[0] >= 0 && modals[1] >= 0) //has meaningful values for 'e' and 't'
+            if(((length + modals[1] - modals[0]) % length) == e2t) //compare delta('e''t') with delta(et)
+                isRot = true;
+
+        return isRot;
     }
-    public static int getE2t(){return e2t;}
+    public int getE2t(){return e2t;}
 }
